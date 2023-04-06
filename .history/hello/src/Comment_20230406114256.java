a52d243dd7f127a19c0b68604fb392352f90c3eb
kim <rkdghkdbfl@gmail.com>
2023-04-06 17:43:06 +0900

    remove

--- .history/hello/src/Comment_20230406114256.java
+++
@@ -1,6 +0,0 @@
-public class Comment {
-    public static void main(String[] args) {
-        // System.out.println("(10분 전) 잠시 후 결혼식이 시작 될 예정이오니 착성 부탁드립니다.");
-        // System.out.println("(5분 전) 잠시 후 결혼식이 시작 될 예정이오니 착성 부탁드립니다.");  // ㅁㅇㄹ
-    }
-}
